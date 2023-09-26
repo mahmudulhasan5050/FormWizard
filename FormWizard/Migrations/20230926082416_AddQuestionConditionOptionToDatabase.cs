34330d35d729735a0c73628984e1ab502c575ee1
mahmudul5050 <mahmudul.rony@outlook.com>
2023-09-27 01:05:41 +0300

    Question Condition and it's option CRU is done.

---
+++ FormWizard/Migrations/20230926082416_AddQuestionConditionOptionToDatabase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FormWizard.Migrations
+{
+    public partial class AddQuestionConditionOptionToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QuestionConditionsOptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConditionOptionText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OptionValue = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderOfDisplay = table.Column<int>(type: "int", nullable: true),
+                    QuestionConditionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QuestionConditionsOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QuestionConditionsOptions_QuestionConditions_QuestionConditionId",
+                        column: x => x.QuestionConditionId,
+                        principalTable: "QuestionConditions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionConditionsOptions_QuestionConditionId",
+                table: "QuestionConditionsOptions",
+                column: "QuestionConditionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QuestionConditionsOptions");
+        }
+    }
+}
---
+++ FormWizard/Pages/QuestionConditionOptions/Edit.cshtml.cs
@@ -0,0 +1,44 @@
+using FormWizard.Data;
+using FormWizard.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+
+namespace FormWizard.Pages.QuestionConditionOptions
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public QuestionConditionOption questionConditionOption { get; set; }
+        [BindProperty]
+
+        public int myFormId { get; set; }
+        [BindProperty]
+
+        public int questionId { get; set; }
+        public void OnGet(int questionconditionoptionid, int questionid, int myformid)
+        {
+            questionConditionOption = _db.QuestionConditionsOptions.Find(questionconditionoptionid);
+            ViewData["QuestionId"] = questionid;
+            ViewData["MyFormId"] = myformid;
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.QuestionConditionsOptions.Update(questionConditionOption);
+                _db.SaveChanges();
+                TempData["success"] = "Question Condition Option has been updated.";
+                return RedirectToPage("Index", new {questionconditionid= questionConditionOption.QuestionConditionId, questionid = questionId, myformid = myFormId });
+            }
+            return Page();
+        }
+    }
+}
